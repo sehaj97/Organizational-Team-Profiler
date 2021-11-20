f5e889ef2fd4c9d82d2c2c7164a5f8d8afad6e6f
sehaj97 <sehajmagan@gmail.com>
2021-11-20 01:24:14 -0500

    added manager html and inputs

--- src/page-template.js
+++ src/page-template.js
@@ -1,20 +1,26 @@
 
-function generateCard(){
+function generateCard(data){
     return `
     <div class="col">
     <div class="card" style="width: 18rem;">
         <div class="card-body bg-primary text-white">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2">Card subtitle</h6>
+            <h5 class="card-title">${data.getName()}</h5>
+            <h6 class="card-subtitle mb-2">${data.getRole()}</h6>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
+                <li class="list-group-item">ID: ${data.id}</li>
+                <li class="list-group-item">ID: ${data.email}</li>
+                <li class="list-group-item">${getFinalInfo(data)}</li>
             </ul>
         </div>
     </div>
     </div>
     `
+}
+
+function getFinalInfo(data){
+    if(data.getRole() === 'Manager'){
+        return `Office Number: ${data.officeNumber}`
+    }
 }
 
 function generateRow(data){
@@ -27,7 +33,7 @@ function generateRow(data){
                 <div class="row m-3 d-flex justify-content-center align-items-center">
                 `
             }
-            html = html + generateCard();
+            html = html + generateCard(data[i]);
             count = i+1;
             if(count%3===0 || data.length === count){
                 html = html + `</div>`;
@@ -59,7 +65,7 @@ module.exports = templateData => {
     </div>
     </nav>
     <div>
-    ${generateRow([1,2,3,4,5,6])}
+    ${generateRow(templateData)}
     </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
